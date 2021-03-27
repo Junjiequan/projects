eb9a6e291d3a83ded5b810af2218cd4bdf07ba7a
mmmG <a331998513@gmail.com>
2021-03-27 01:45:27 +0100

    desktop only

--- fylo-data-storage-component-master/index.js
+++ fylo-data-storage-component-master/index.js
@@ -1,8 +1,10 @@
 const count = document.querySelector('.t-h1');
 const progress = document.querySelector('.progress')
+const count2 = document.getElementById('number')
 
 const changeSpace = ()=>{
     count.innerText = Math.floor(Math.random() *1000); 
+    count2.innerText = '1000' - count.innerText + ' GB';
     progress.style.width= count.innerText / 10 + '%';
         setTimeout(changeSpace,5000);
 }
