b1740bce69b22d081cb0bca2e1ea5a0f76336d60
mmmG <a331998513@gmail.com>
2021-04-08 14:23:43 +0200

    70%

--- crowdfunding-product-page-main/app.js
+++ crowdfunding-product-page-main/app.js
@@ -1,7 +1,10 @@
-const bookMark = document.querySelector('.no-hover');
-const mobileMenu = document.querySelector('.side-links')
-const menuIcon = document.querySelector('.menu-icon')
-const iconIMG = menuIcon.childNodes[0]
+const bookMark = document.querySelector('.bookmark');
+const mobileMenu = document.querySelector('.side-links');
+const menuIcon = document.querySelector('.menu-icon');
+const iconIMG = menuIcon.childNodes[0];
+const selectBox = document.querySelector('.popup');
+const isProject = document.querySelector('#project');
+console.log(selectBox)
 //bookmark
 const getMark = () =>{
     if(bookMark.innerText == 'Bookmark'){
@@ -30,7 +33,30 @@ const closeMenu = () => {
     mobileMenu.style.display= "none"
     iconIMG.src = './images/icon-hamburger.svg';
 }
+const getNumberbox = (e) =>{
+    const target = e.target;
+    const inputRadio = document.querySelector('.input-box');
+    const popup = document.querySelector('.popup');
+    const block = document.querySelector('.block')
+    if(target.checked == true){
+        inputRadio.style.opacity = "1";
+        inputRadio.classList.add('show');
+    } 
+    if(target.id == 'close'){
+        popup.style.display = "none"
+        block.style.display = "none"
+    }
+}
+const openProject = () =>{
+    const popup = document.querySelector('.popup');
+    const block = document.querySelector('.block');
+    popup.style.display= "flex";
+    block.style.display = "flex"
+}
+
 document.addEventListener('DOMContentLoaded',checkMark);
 bookMark.addEventListener('click', getMark);
 mobileMenu.addEventListener('click', closeMenu);
-menuIcon.addEventListener('click', getMenu)
+menuIcon.addEventListener('click', getMenu);
+selectBox.addEventListener('click',getNumberbox);
+isProject.addEventListener('click', openProject)
