137b9f6e7e6d7bb11c8c0e7921fbffb262fabeaf
mmmG <a331998513@gmail.com>
2021-04-08 10:22:46 +0200

    60%

--- crowdfunding-product-page-main/app.js
+++ crowdfunding-product-page-main/app.js
@@ -1,4 +1,7 @@
 const bookMark = document.querySelector('.no-hover');
+const mobileMenu = document.querySelector('.side-links')
+const menuIcon = document.querySelector('.menu-icon')
+const iconIMG = menuIcon.childNodes[0]
 //bookmark
 const getMark = () =>{
     if(bookMark.innerText == 'Bookmark'){
@@ -11,11 +14,23 @@ const getMark = () =>{
         localStorage.clear('Mark','');
     }
 }
-const checkMark = ()=>{
+const checkMark = () => {
     if(localStorage.getItem('Mark') == 'bookmarked'){
         bookMark.classList.toggle('bookmarked');
         bookMark.innerText = 'Bookmarked';
     }
 }
+const getMenu = () =>{
+    if(mobileMenu.style.display= "none"){
+        iconIMG.src = './images/icon-close-menu.svg';
+        mobileMenu.style.display= "flex"
+    }
+}
+const closeMenu = () => {
+    mobileMenu.style.display= "none"
+    iconIMG.src = './images/icon-hamburger.svg';
+}
 document.addEventListener('DOMContentLoaded',checkMark);
 bookMark.addEventListener('click', getMark);
+mobileMenu.addEventListener('click', closeMenu);
+menuIcon.addEventListener('click', getMenu)
