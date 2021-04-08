ea08134a2af027d9707ddcd066abe6a14ed4eea8
mmmG <a331998513@gmail.com>
2021-04-08 04:38:42 +0200

    40%

--- crowdfunding-product-page-main/app.js
+++ crowdfunding-product-page-main/app.js
@@ -4,11 +4,18 @@ const getMark = () =>{
     if(bookMark.innerText == 'Bookmark'){
         bookMark.classList.toggle('bookmarked');
         bookMark.innerText = 'Bookmarked';
+        localStorage.setItem('Mark','bookmarked')
     }  else {
         bookMark.classList.remove('bookmarked');
         bookMark.innerText = 'Bookmark';
+        localStorage.clear('Mark','');
     }
-
 }
-
+const checkMark = ()=>{
+    if(localStorage.getItem('Mark') == 'bookmarked'){
+        bookMark.classList.toggle('bookmarked');
+        bookMark.innerText = 'Bookmarked';
+    }
+}
+document.addEventListener('DOMContentLoaded',checkMark);
 bookMark.addEventListener('click', getMark);
