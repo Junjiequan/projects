f0db6d9395d8ae762a4acd800dff045f38cb1364
mmmG <a331998513@gmail.com>
2021-04-13 14:07:14 +0200

    added append function

--- url-shortening-api-master/app.js
+++ url-shortening-api-master/app.js
@@ -1,7 +1,34 @@
 const urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+const linkError = document.querySelector('.warning-zone')
+const urlInput = document.querySelector('.link-input');
+const ulContainer = document.querySelector('.shorten-list')
 
 
-document.querySelector('.url-btn').addEventListener('click', ()=>{
-    document.querySelector('.warning-zone').innerText = 'Please add a link';
-    document.querySelector('.warning-zone').style.opacity= 1;
+document.querySelector('.url-btn').addEventListener('click', ()=>{    
+    if(urlInput.value.match(urlRegex)){
+        const li = document.createElement('li');
+        li.classList.add('shorten-link');
+
+        const url = document.createElement('div');
+        url.classList.add('links');
+
+        const urlBefore = document.createElement('div');
+        urlBefore.classList.add('before');
+        urlBefore.innerText = urlInput.value;
+
+        const urlAfter = document.createElement('div');
+        urlAfter.classList.add('after');
+        urlAfter.innerText = ('put API here')
+        url.appendChild(urlBefore);
+        url.appendChild(urlAfter);
+        li.appendChild(url);
+        ulContainer.appendChild(li);
+        
+
+        //remove text
+        urlInput.value = '';
+    } else {
+        linkError.innerText = 'Please add a link';
+        linkError.style.opacity= 1;
+    }
 })
