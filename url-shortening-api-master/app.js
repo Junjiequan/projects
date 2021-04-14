3b584f28c511619023a0036b7aefa9a5d3d299b6
mmmG <a331998513@gmail.com>
2021-04-14 18:08:41 +0200

    done!

--- url-shortening-api-master/app.js
+++ url-shortening-api-master/app.js
@@ -1,35 +1,52 @@
+
+//preserve previous position
+history.pushState(null, null, ' ')
+// history.replaceState(null, null, ' ');
+
+//selectors
 const urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 const linkError = document.querySelector('.warning-zone');
 const urlInput = document.querySelector('.link-input');
 const ulContainer = document.querySelector('.shorten-list');
+const navIcon = document.querySelector('.real-burger');
+const navBox = document.querySelector('.top-nav-wrapper');
 
 
-
-document.querySelector('.url-btn').addEventListener('click', ()=>{    
+//fetchData
+const getUrl = (url) =>{
+    return fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+            .then (resp => resp.json())
+            .then (data => data.result)
+}
+//getButton
+document.querySelector('.url-btn').addEventListener('click', async ()=>{    
     if(urlInput.value.match(urlRegex)){
         //remove error border
         linkError.innerText = '';
         linkError.style.opacity= 0;
         urlInput.classList.remove('error-border')
+        document.querySelector('.form-shorten').style.pointerEvents = 'none';
+        //start fetching data
+        document.querySelector('.url-btn').innerText = 'Loading...';
+        document.querySelector('.url-btn').classList.add('loading');
+        // const getLink =  await getUrl(urlInput.value)
 
+        //create container for generated links
         const li = document.createElement('li');
-        //links
-        const url = document.createElement('div');
-        url.classList.add('links');
+
+        //div for links
+        const urlLink = document.createElement('div');
+        urlLink.classList.add('links');
 
         const urlBefore = document.createElement('div');
         urlBefore.classList.add('before');
-        urlBefore.innerText = urlInput.value
+        urlBefore.innerText = getLink.original_link //fetch data
         
         const urlAfter = document.createElement('div');
         urlAfter.classList.add('after');
-        urlAfter.innerText = ('put API here');
+        urlAfter.innerText = getLink.short_link2;   //fetch data
 
-        //this/////////////////////////////////////////////////
-        //data part
-        //this/////////////////////////////////////////////////
-        
-        //buttons
+        //div for buttons
         const btn = document.createElement('div');
         btn.classList.add('buttons')
 
@@ -41,22 +58,27 @@ document.querySelector('.url-btn').addEventListener('click', ()=>{
         btnRemove.innerHTML = `<i class="far fa-trash-alt"></i>`
         btnRemove.classList.add('li-remove');
 
-
-        url.appendChild(urlBefore);
-        url.appendChild(urlAfter);
+        //append childs
+        urlLink.appendChild(urlBefore);
+        urlLink.appendChild(urlAfter);
         btn.appendChild(btnCopy);
         btn.appendChild(btnRemove);
-        li.appendChild(url);
+        li.appendChild(urlLink);
         li.appendChild(btn);
         li.classList.add('shorten-link');
         ulContainer.appendChild(li);
         
+        //popup animation
         setTimeout(()=>{
             li.style.opacity = 1;
             li.style.transform = "scaleX(1)"
         },100)
+
         //remove text
         urlInput.value = '';
+        document.querySelector('.url-btn').innerText = 'Shorten it!';
+        document.querySelector('.url-btn').classList.remove('loading');
+        document.querySelector('.form-shorten').style.pointerEvents = 'auto';
     } else {
         linkError.innerText = 'Please add a link';
         linkError.style.opacity= 1;
@@ -64,6 +86,7 @@ document.querySelector('.url-btn').addEventListener('click', ()=>{
     }
 })
 
+//onClick for <li> buttons
 ulContainer.addEventListener('click',(e)=>{
     const target = e.target
     const targetLi = target.closest('.shorten-link')
@@ -79,6 +102,7 @@ ulContainer.addEventListener('click',(e)=>{
         if(target.innerText == 'Copy'){
             target.classList.add('li-copied')
             target.innerText = 'Copied';
+            
             //Creating copy environment.
             const textCopy = targetLi.querySelector('.after').innerText;
             const copyCondition = document.createElement('input');
@@ -95,7 +119,46 @@ ulContainer.addEventListener('click',(e)=>{
         } 
     }
 })
-// history.replaceState(null, null, ' ');
-history.pushState(null, null, ' ')
 
+//onClick for mobile nav menu icon
+let isClick = true;
+document.addEventListener('click',(e)=>{
+    const getDisplay = getComputedStyle(navBox).getPropertyValue('display')
+    const target = e.target;
+    //setClick frequency to prevent fast click
+    if(isClick){
+        isClick = false;
+        setTimeout(()=>{
+            isClick=true;
+        },500)
+        if( target.classList[0] == 'real-burger'){
+            navIcon.classList.add('rotate');
+            setTimeout(()=>{
+                navIcon.classList.remove('rotate')
+            },500)
+            if(getDisplay == 'none'){
+                navBox.style.display = 'flex';
+                setTimeout(()=>{
+                    navBox.style.transform = 'translateX(0)'
+                },100)
+            } else {
+                navBox.style.transform = 'translateX(-140%)'
+                setTimeout(()=>{
+                    navBox.style.display = 'none';
+                },500)
+            }
+        } else {
+            navBox.style.transform = 'translateX(-140%)'
+            setTimeout(()=>{
+                navBox.style.display = 'none';
+            },500)
+        }
+    }
+})
 
+window.addEventListener('resize', ()=>{
+    if(window.innerWidth >= 841){
+        navBox.style.display = 'flex';
+        navBox.style.transform = 'translateX(0)'
+    }
+});
