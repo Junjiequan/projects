003c83279be3a7295f28b81875758e2bb3cd420e
mmmG <a331998513@gmail.com>
2021-04-20 20:54:04 +0200

    done.

--- ip-address-tracker-master/main.js
+++ ip-address-tracker-master/main.js
@@ -1,25 +1,45 @@
+//selectors
 const ipOutput = document.querySelector('[data-id = "ip"]').childNodes[3];
 const cityOutput = document.querySelector('[data-id = "location"]').childNodes[3];
-const postOutput = document.querySelector('[data-id = "location"]').childNodes[5];
+const postOutput = document.querySelector('[data-id = "location"]').childNodes[3].childNodes[1];
 const timeOutput = document.querySelector('[data-id = "timezone"]').childNodes[3];
 const ispOutput = document.querySelector('[data-id = "isp"]').childNodes[3];
+const loading = document.querySelector('.map__loading');
 const form = document.querySelector('form');
 const input = document.querySelector('input[type=text]');
-
-
-
-
-async function getData(ip){
-    const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_UTDYv4fFYO4pdElyoGoCeSGOn1FB2&ipAddress=${ip}`);
-    console.log(res)
-    if(!res.ok)
-        throw new Error('Check IP address');
-    data = await res.json();
-    displayData(data);
-}
-//selectors
-const dataOutput = document.querySelectorAll('.hero__data__block');
+const ipReg = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 //functions
+async function init(ip) {
+    try{
+        const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_UTDYv4fFYO4pdElyoGoCeSGOn1FB2&ipAddress=${ip}`);
+        if(!res.ok)
+            throw new Error('Invalid IP address');
+        data = await res.json();
+        displayData(data);
+        isLoading();
+    } catch(err){
+        input.setAttribute('id', 'invalid');
+        input.value = `${err}`
+    }
+}
+init('');  // <<<<<<<<<<init
+
+// loading animation
+const isLoading =()=>{
+    if(loading.style.display != 'flex'){
+        loading.style.display = 'flex'
+        setTimeout(()=>{
+            loading.style.opacity = '1'
+        },100);
+    }else{
+        setTimeout(()=>{
+            loading.style.opacity = '0';
+            setTimeout(()=> loading.style.display = 'none',500)
+        },1000)
+    };
+}
+
+let mymap = null;
 
 const displayData = (data)=>{
     ipOutput.innerText = data.ip;
@@ -27,12 +47,43 @@ const displayData = (data)=>{
     postOutput.innerText = data.location.postalCode;
     timeOutput.innerText = data.location.timezone;
     ispOutput.innerText = data.isp;
-    // getData()
-    //     .catch((e)=>  document.querySelector('input[type=text]').value = e)
+    if(mymap != undefined && mymap != null){
+        mymap.remove();
+    }
+    displayMap(data.location.lat, data.location.lng);
+    isLoading();
 }
 
+// map API
+const displayMap = (x,y) =>{
+    mymap = L.map('mapid',{ zoomControl: false}).setView([x, y], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a> ' + '<a style="color:red" href="https://github.com/a331998513/projects">Jay</a> was here',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
+
+    let greenIcon = L.icon({
+        iconUrl: './images/icon-location.svg',
+        iconSize: [36, 46],
+    });
+    L.marker([x, y], {icon: greenIcon}).addTo(mymap);
+    L.control.zoom({ position: 'topleft'}).addTo(mymap);
+}
+//eventListener
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const formData = new FormData(e.target)
-    getData(formData.get('searchIP'));
+    init(formData.get('searchIP'));
+    isLoading();
 })
+input.addEventListener('click', ()=>{
+    if(!ipReg.test(input.value)){
+        input.setAttribute('id','');
+        input.value = '';
+    }
+})
+
