6bcd959eb166ebd777cc2a6ecd77d7e400629409
mmmG <a331998513@gmail.com>
2021-04-20 09:20:18 +0200

    Ip part is nearly done

--- ip-address-tracker-master/main.js
+++ ip-address-tracker-master/main.js
@@ -1,25 +1,38 @@
-async function getData(){
-    const res = await fetch('https://geo.ipify.org/api/v1?apiKey=at_UTDYv4fFYO4pdElyoGoCeSGOn1FB2&ipAddress=8.8.8.8');
+const ipOutput = document.querySelector('[data-id = "ip"]').childNodes[3];
+const cityOutput = document.querySelector('[data-id = "location"]').childNodes[3];
+const postOutput = document.querySelector('[data-id = "location"]').childNodes[5];
+const timeOutput = document.querySelector('[data-id = "timezone"]').childNodes[3];
+const ispOutput = document.querySelector('[data-id = "isp"]').childNodes[3];
+const form = document.querySelector('form');
+const input = document.querySelector('input[type=text]');
+
+
+
+
+async function getData(ip){
+    const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_UTDYv4fFYO4pdElyoGoCeSGOn1FB2&ipAddress=${ip}`);
+    console.log(res)
     if(!res.ok)
-        throw new Error('Something is not going well with getting data.');
+        throw new Error('Check IP address');
     data = await res.json();
     displayData(data);
 }
-getData()
-    .catch(()=> console.log('fetching process went wrong'))
-
 //selectors
 const dataOutput = document.querySelectorAll('.hero__data__block');
 //functions
-const getIp = (ip) =>{
-console.log('this', ip)
-}
+
 const displayData = (data)=>{
-    return;
+    ipOutput.innerText = data.ip;
+    cityOutput.innerText = data.location.city;
+    postOutput.innerText = data.location.postalCode;
+    timeOutput.innerText = data.location.timezone;
+    ispOutput.innerText = data.isp;
+    // getData()
+    //     .catch((e)=>  document.querySelector('input[type=text]').value = e)
 }
 
-document.querySelector('form').addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const formData = new FormData(e.target)
-    getIp(formData.get('searchIP'));
+    getData(formData.get('searchIP'));
 })
