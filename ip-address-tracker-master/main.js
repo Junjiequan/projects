40c39af07d427cbee7a8a80870f969a2e8b05edd
mmmG <a331998513@gmail.com>
2021-04-20 21:31:32 +0200

    fixed map zoom controll location and footer size

--- ip-address-tracker-master/main.js
+++ ip-address-tracker-master/main.js
@@ -58,7 +58,7 @@ const displayMap = (x,y) =>{
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a> ' + '<a style="color:red" href="https://github.com/a331998513/projects">Jay</a> was here',
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1
@@ -69,7 +69,7 @@ const displayMap = (x,y) =>{
         iconSize: [36, 46],
     });
     L.marker([x, y], {icon: greenIcon}).addTo(mymap);
-    L.control.zoom({ position: 'topleft'}).addTo(mymap);
+    L.control.zoom({ position: 'bottomright'}).addTo(mymap);
 }
 //eventListener
 form.addEventListener('submit', (e)=>{
