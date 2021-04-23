b2f15029e270a0693168aba64d879314974f8303
mmmG <a331998513@gmail.com>
2021-04-23 23:59:56 +0200

    continues tomorrow

--- launch-countdown-timer-main/main.js
+++ launch-countdown-timer-main/main.js
@@ -1,25 +1,51 @@
-const number = document.querySelector('.hero__timer__number');
-const numberPrev = document.querySelector('.number__front');
-const numberNext = document.querySelector('.number__behind')
-const numberFlip = document.querySelector('.hero__timer__number__bg')
+//selectors // container
+const time = document.querySelectorAll('.hero__timer__number')
+//selectors // value
+    let secondsPrev = +time[3].dataset.numberBefore
+    let secondsNext = +time[3].dataset.numberAfter
+    let minutesPrev = +time[2].dataset.numberBefore
+    let minutesNext = +time[2].dataset.numberAfter
+    let hoursPrev = +time[1].dataset.numberBefore
+    let hoursNext = +time[1].dataset.numberAfter
+    let daysPrev = +time[0].dataset.numberBefore
+    let daysNext = +time[0].dataset.numberAfter
+    secondsPrev = 10;
+    secondsNext = 10;
 
-function countSecond(){
-        number.dataset.numberBefore--;
-        number.dataset.numberAfter--;
-        if(number.dataset.numberAfter == 0){
-            number.dataset.numberAfter = 59;
-        };
-        if(number.dataset.numberBefore == 0){
-            number.dataset.numberBefore = 59
-        };
-        if(number.dataset.numberAfter < 10 && number.dataset.numberBefore < 10){
-            number.dataset.numberAfter = '0' + number.dataset.numberAfter
-            number.dataset.numberBefore = '0' + number.dataset.numberBefore
-        };
-        numberFlip.classList.toggle('flipped')
-        numberPrev.textContent = number.dataset.numberBefore;
-        numberNext.textContent = number.dataset.numberAfter;
-}
+const secondsFront = time[3].querySelectorAll('.number')[0];
+const secondsBehind = time[3].querySelectorAll('.number')[1];
+//functions
+(function runTime(){
+    secondsFront.parentElement.addEventListener('transitionend', ()=>{
+        secondsFront.parentElement.classList.add('no-animation')
+        secondsFront.parentElement.classList.remove('flipped')
+    })
 
+    if(!secondsFront.parentElement.classList.contains('flipped')){
+        secondsFront.parentElement.classList.remove('no-animation')
+        secondsFront.parentElement.classList.add('flipped')
+    }
+    secondsPrev--;
+    secondsNext--;
+    if( secondsPrev < 10 ){
+        secondsPrev = '0' + secondsPrev;
+    }
+    if(secondsNext < 10){
+        secondsNext = '0' + secondsNext;
+    }
+    if( secondsPrev == 0 || secondsNext ==0 ){
+        if(secondsPrev) secondsPrev = 59;
+        if(secondsNext) secondsNext = 59;
+    }
 
-setInterval(countSecond,1000);
+    secondsFront.textContent = secondsNext;
+    secondsBehind.textContent = secondsPrev;
+    time[3].dataset.numberBefore = secondsPrev
+    time[3].dataset.numberAfter = secondsNext
+
+    
+    setTimeout(runTime,1000)
+    
+    
+})();
+
