81bcb879340238bfe2d53026e309dd1b10cb3921
mmmG <a331998513@gmail.com>
2021-05-08 05:06:11 +0200

    all done except mobile drag

--- todo-app-main/js/app.js
+++ todo-app-main/js/app.js
@@ -1,28 +1,23 @@
-import {preData} from './data.js';
 //selectors
 const body = document.body;
 const bodyBg = document.querySelector('.bg')
 const colorTrigger = document.querySelector('.app__switch');
 const createTodo = document.querySelector('.check');
-const deleteTodo = document.querySelector('.delete');
 const todoInput = document.querySelector('[data-id = "todo-content"]')
 const form = document.querySelector('.todo__form')
 const list = document.querySelector('.todo__list');
-const items = document.querySelectorAll('.todo__item');
+
 const filterBox = document.querySelector('.todo__bottom');
-const filterAll = document.querySelector('[data-id=all]');
-const filterActive = document.querySelector('[data-id=active]');
-const filterCompleted = document.querySelector('[data-id=completed]');
 const dancingMeme = document.querySelector('.meme__removal');
-const hasTodo = Array.from(list.children).slice(1,list.children.length).length
+
 let todoLeft = 0;
 
 
 //functions
-const createItem = (todoText,scale,height)=>{
+const createItem = (todoText,scale,height,index)=>{
    const uniqueId = Math.floor(Math.random() * 100000)
    list.insertAdjacentHTML('beforeend',`
-   <li class="todo__item" draggable="true" ondragstart="dragstart(event)" ondragenter="dragenter(event)" ondragend="dragend(event)" style="transform:scaleY(${scale}); height:${height};" id="${uniqueId}" >
+   <li class="todo__item" draggable="true" ondragstart="dragstart(event)" ondragenter="dragenter(event)" ondragend="dragend(event)" style="transform:scaleY(${scale || '0'}); height:${height || '0'};" id="${uniqueId}" >
    <button class="check-mark" aria-label="todo-check" data-id="check" ></button>
    <input class="active" placholder="created" type="text" aria-label="todo-item" name="todo-item" value="${todoText}" disabled>
    <button class="delete" aria-label="todo-delete" data-id="delete"></button>
@@ -36,29 +31,11 @@ const createItem = (todoText,scale,height)=>{
       updateTodoLeft(todoLeft);
       createMeme(todoLeft);
    },10)
+   setLocalTodo(todoText,'')
 }
-if(hasTodo === 0){
-   preData.forEach(data=>{
-      createItem(data,'1',' 6.5rem')
-   })
-}
-// preData.map(data =>{
-//    list.insertAdjacentHTML('beforeend',`
-//    <li class="todo__item" draggable="true" ondragstart="dragstart(event)" ondragenter="dragenter(event)" ondragend="dragend(event)" style="transform:scaleY(1); height:6.5rem;" id="${todoLeft}" >
-//    <button class="check-mark" aria-label="todo-check" data-id="check" ></button>
-//    <input class="active" placholder="created" type="text" aria-label="todo-item" name="todo-item" value="${data}" disabled>
-//    <button class="delete" aria-label="todo-delete" data-id="delete"></button>
-//    </li>
-//    `)
-//    todoInput.value = '';
-//    todoLeft++;
-//    updateTodoLeft(todoLeft);
-//    createMeme(todoLeft);
-// })
-
-console.log()
 const deleteItem = (todo)=>{
    const targetTodo = todo.closest('.todo__item')
+   const targetTodoText = targetTodo.children[1].value
    targetTodo.style.transform = "scaleY(0)";
    targetTodo.style.height = "0";
    setTimeout(()=>{
@@ -67,6 +44,7 @@ const deleteItem = (todo)=>{
    todoLeft--;
    updateTodoLeft(todoLeft);
    createMeme(todoLeft);
+   deleteLocalTodo(targetTodoText);
 }
 
 function updateTodoLeft(amount){
@@ -75,13 +53,41 @@ function updateTodoLeft(amount){
 
 const toggleCheck = (todo)=>{
    todo.classList.toggle('checked');
+   const text = todo.children[1].value;
+   const style = todo.classList[1] ? todo.classList[1] : '';
+   updateLocalTodo(text,style)
+}
+function filterCheck(filterCheck){
+   const filter = document.querySelector('.todo__bottom-actions').children
+   const filters = [filter[0],filter[1],filter[2]]
+   filters.forEach((elem)=>{
+      elem.style.color = '';
+      if(elem.dataset.id === filterCheck){
+         if(elem.dataset.id  === 'all') {
+            elem.style.color = "hsl(220, 98%, 61%)"
+         }
+         if(elem.dataset.id  === 'active'){
+            if(body.classList.contains('dark')){
+               elem.style.color = "hsl(236, 33%, 92%)"
+            } else {
+               elem.style.color = "hsl(237, 14%, 26%)";
+            }
+         }
+         if(elem.dataset.id  === 'completed'){
+            
+            if(body.classList.contains('dark')){
+               elem.style.color = "hsl(236, 33%, 92%)"
+            } else elem.style.color = "hsl(237, 14%, 26%)"
+         } 
+      }
+   })
 }
 
 const todoFilter = (target) =>{
    const todos = document.querySelectorAll('.todo__item');
-   const filter = target.dataset.id;
+   const filterId = target.dataset.id;
    todos.forEach(item =>{
-      switch (filter){
+      switch (filterId){
          case 'all':
             item.style.display = "flex";
             break;
@@ -100,40 +106,67 @@ const todoFilter = (target) =>{
                item.remove();
                todoLeft--;
                updateTodoLeft(todoLeft);
+               deleteLocalTodo(item.children[1].value)
             }
             break;
-         default:
-            document.querySelector('[data-id=all]').focus()
-            item.style.display = "flex";
       }
    })
 }
 //localStorage 
-const setLocalTodo = (data, style) =>{
-   let storage;
-
+function setLocalTodo(data, style){
+   let todos ;
+   if(localStorage.getItem('todos') === null) todos = [];
+   else todos = JSON.parse(localStorage.getItem('todos'))
+   
+   todos.push([data,style])
+   localStorage.setItem('todos',JSON.stringify(todos));
+}
+const updateLocalTodo = (data,style,target) =>{
+   Array.prototype.move = function(from,to){
+      this.splice(to, 0, this.splice(from, 1)[0])
+   }
+   let todos = JSON.parse(localStorage.getItem('todos'))
+   todos.forEach((item,index,arrays)=>{
+      if(item[0] === data){
+         item[1] = style;
+         arrays.move(index,target)
+         localStorage.setItem('todos', JSON.stringify(todos));
+      }
+   })
+}
+function getLocalTodo(){
+   let todos ;
+   if(localStorage.getItem('todos') === null) todos = [];
+   else todos = JSON.parse(localStorage.getItem('todos'))
+   todos.forEach(data=>{
+      const text = data[0];
+      const style = data[1];
+      const uniqueId = Math.floor(Math.random() * 100000)
+      list.insertAdjacentHTML('beforeend',`
+      <li class="todo__item ${style}" draggable="true" ondragstart="dragstart(event)" ondragenter="dragenter(event)" ondragend="dragend(event)" style="transform:scaleY(1); height:6.5rem;" id="${uniqueId}" >
+      <button class="check-mark" aria-label="todo-check" data-id="check" ></button>
+      <input class="active" placholder="created" type="text" aria-label="todo-item" name="todo-item" value="${text}" disabled>
+      <button class="delete" aria-label="todo-delete" data-id="delete"></button>
+      </li>
+      `)
+      todoLeft++;
+      updateTodoLeft(todoLeft);
+      createMeme(todoLeft);
+   })
+}
+function deleteLocalTodo(text){
+   let todos ;
+   if(localStorage.getItem('todos') === null) todos = [];
+   else todos = JSON.parse(localStorage.getItem('todos'))
+   todos.forEach((item,index,object)=>{
+      if(item[0] === text){
+         object.splice(index,1)
+         localStorage.setItem('todos', JSON.stringify(todos));
+      }
+   })
 }
 
-const updateLocalTodo = () =>{
-
-}
-const getLocalTodo = () =>{
-}
-const deleteLocalTodo = (target) =>{
-
-}
 //dragevents
-// const isDrag = () =>{
-//    const todos = document.querySelectorAll('.todo__item')
-//    todos.forEach(index=>{
-//       index.setAttribute('draggable',true);
-//       index.setAttribute('ondragstart',"dragstart(event)");
-//       index.setAttribute('ondragenter','dragenter(event)');
-//       index.setAttribute('ondragend','dragend(event)');
-//    })
-// }
-
-// isDrag();
 let draggedItem;
 window.dragstart = function dragstart(event){
    event.dataTransfer.effectAllowed = "copy";
@@ -161,11 +194,16 @@ window.dragenter = function dragenter(event){
  window.dragend = function dragend(event){
    event.dataTransfer.dropEffect = "copy"
    event.target.className = event.target.className.replace(' dragged', '');
+   const target = event.target
+   const todoArrays = Array.from(list.querySelectorAll('.todo__item'))
+   const changedIndex = todoArrays.indexOf(target)
+   const changedTargetText = target.children[1].value
+   const changedTargetStyle = target.classList[1] ? target.classList[1] : '';
+   updateLocalTodo(changedTargetText,changedTargetStyle,changedIndex)
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-//This part is highlighted as I have no idea why 
-//list.addEventListener(dragevents) cause errors with HTML, although it works..
+//This part is highlighted as I have no idea why list.addEventListener(dragevents) cause errors with HTML, although it works..
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 // list.addEventListener('dragstart', dragstart)
@@ -174,9 +212,23 @@ window.dragenter = function dragenter(event){
 list.addEventListener('dragover', (e)=>{ e.preventDefault();}, false);
 list.addEventListener('dragleave', (e)=>{ e.preventDefault();}, false);
 
+//preventing drag buttons!
+list.addEventListener('mousedown', (e)=>{
+   if(e.target.type === 'submit'){
+     e.target.parentNode.setAttribute('draggable', false)
+     setTimeout(()=>{
+      e.target.parentNode.setAttribute('draggable', true)
+     },300)
+   }
+});
+
+
 
 //eventListeners
-// document.addEventListener('DOMContentLoaded', getLocalTodo)
+document.addEventListener('DOMContentLoaded', ()=>{
+   getLocalTodo();
+   document.querySelector('.todo__bottom-actions--all').style.color = "hsl(220, 98%, 61%)"
+})
 filterBox.addEventListener('click', (e) => todoFilter(e.target));
 
 form.addEventListener('click',(e)=>{
@@ -198,6 +250,17 @@ form.addEventListener('click',(e)=>{
          deleteItem(target);
       }
    };
+   if(target.parentNode.className === 'todo__bottom-actions'){
+      if(target.dataset.id === 'all'){
+         filterCheck(target.dataset.id)
+      }
+      if(target.dataset.id === 'active'){
+         filterCheck(target.dataset.id)
+      }
+      if(target.dataset.id === 'completed'){
+         filterCheck(target.dataset.id)
+      }
+   }
    createMeme();
 });
 todoInput.addEventListener('keyup',(e)=>{
--- todo-app-main/js/data.js
+++
@@ -1,7 +0,0 @@
-export  const preData = [
-    'Play games for 5 hours',
-    'Do something for 8 hours',
-    'Sleep 6 hours',
-    'eat junk foods',
-    'Coding straight for 10 hours as if having no other life'
-]
